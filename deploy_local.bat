31063137baa674bbc7984d3de01ec267e9718fa2
Florian Enner <florian@enner.org>
2013-12-13 06:27:54 -0500

    fixed call to maven execution script
    otherwise mvn only gets called once and then stops.

--- deploy_local.bat
+++ deploy_local.bat
@@ -1,3 +1,4 @@
-mvn -DaltDeploymentRepository=github.ennerf::default::file:mvn-repo/releases/ clean deploy
 cd opencv
-mvn deploy:deploy-file -Durl=file:../mvn-repo/releases/ -Dfile=opencv-247.jar -DpomFile=pom.xml 
+call mvn deploy:deploy-file -Durl=file:../mvn-repo/releases/ -Dfile=opencv-247.jar -DpomFile=pom.xml
+cd ..
+call mvn -DaltDeploymentRepository=github.ennerf::default::file:mvn-repo/releases/ clean deploy
