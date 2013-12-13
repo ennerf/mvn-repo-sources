e9cb8b52ec8329dbafe0b8082a7991089ac38866
Florian Enner <florian@enner.org>
2013-12-13 12:13:54 -0500

    added generated source for OpenCV 2.4.7
    The source was generated on Ubuntu 12.04 x64

--- deploy_local.bat
+++ deploy_local.bat
@@ -1,4 +1,2 @@
-cd opencv
-call mvn deploy:deploy-file -Durl=file:../mvn-repo/releases/ -Dfile=opencv-247.jar -DpomFile=pom.xml
-cd ..
 call mvn -DaltDeploymentRepository=github.ennerf::default::file:mvn-repo/releases/ clean deploy
+rmdir /s /q mvn-repo\releases\dummy
